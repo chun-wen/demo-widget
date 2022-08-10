c1e397b1f469d526ba12cec3bd17bcd7f0f1df61
TW-Web-Andy Yu <andy.yu@shoalter.com>
2022-08-10 15:34:09 +0800

    feat: add constant

---
+++ src/components/Widget/components/constants/index.js
@@ -0,0 +1,10 @@
+const DEFAULT_WORD = {
+    connectionFailZh_HK: `MallMall查而家未Ready呀!`,
+    connectionFailEn: `MallmallCha is not available at the moment`,
+    connectionFailRetryButtonZh_HK: `立即重試`,
+    connectionFailRetryButtonEn: `Retry`,
+    inputTextFieldHintZh_HK: `請輸入你既問題…`,
+    inputTextFieldHintEn: `Type a message…`,
+};
+
+export default DEFAULT_WORD;
