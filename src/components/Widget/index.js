d4ded439ed62f99cc53e5545cc468e0b880b3276
Josh Chou <josh.chou@shoalter.com>
2022-08-19 14:47:33 +0800

    fix: fix request messages logic

--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -118,10 +118,9 @@ class Widget extends Component {
   getSessionId() {
     const { storage } = this.props;
     // Get the local session, check if there is an existing session_id
-    // handle ios lost localStorage issue
-    const localSession = getLocalSession(storage, SESSION_NAME)
+    const localSession = getLocalSession(storage, SESSION_NAME);
     const localId = localSession ? localSession.session_id : null;
-    return Cookies.get('_sessionID') && this.props.isSameUser ? Cookies.get('_sessionID') : localId;
+    return localId;
   }
 
   sendMessage(payload, text = '', when = 'always', tooltipSelector = false) {
@@ -369,7 +368,8 @@ class Widget extends Component {
       initialized,
       connectOn,
       tooltipPayload,
-      tooltipDelay
+      tooltipDelay,
+      isSameUser
     } = this.props;
     if (!socket.isInitialized()) {
       socket.createSocket();
@@ -381,14 +381,15 @@ class Widget extends Component {
       });
 
       this.checkVersionBeforePull();
+      // dispatch(pullSession());
+      storage.clear();
 
-      dispatch(pullSession());
 
-      const userId = Cookies.get('_userID') || '';
-      // Request a session from server
       socket.on('connect', () => {
+        // Cookies value is from web cookies' muid
+        const userId = Cookies.get('_userID') || '';
         const localId = this.getSessionId() || null;
-        // empty string or user-id
+        // Request a session from server
         socket.emit('session_request', { session_id: localId, user_id: userId });
       });
 
@@ -402,29 +403,22 @@ class Widget extends Component {
         this.setState({
           remoteId
         })
+        console.log(`this.state.remoteId:${this.state.remoteId}`);
         dispatch(connectServer());
         /*
         Check if the session_id is consistent with the server
         If the localId is null or different from the remote_id,
         start a new session.
         */
-        const localId = this.getSessionId();
-        if (localId !== remoteId) {
-
-          storage.clear();
+        if (!isSameUser) {
+          if (sendInitPayload) {
+            this.trySendInitPayload();
+          }
         }
         if (connectOn === 'mount' && tooltipPayload) {
           this.tooltipTimeout = setTimeout(() => {
             this.trySendTooltipPayload();
           }, parseInt(tooltipDelay, 10));
-        }
-        storeLocalSession(storage, SESSION_NAME, remoteId);
-        if (userId) {
-          Cookies.set('_sessionID', remoteId);
-        }
-        dispatch(pullSession());
-        if (sendInitPayload) {
-          this.trySendInitPayload();
         }
       });
 
